b9e7d8a07e8803fb39511d1493ac04db7d99a176
cliksmodernos@gmail.com <cliksmodernos@gmail.com@cba955b3-617a-336c-4ae1-095d0d3084ae>
2010-09-14 20:27:59 +0000

    Agregadas esas funciones!

---
+++ trunk/TP2/applyMask.m
@@ -0,0 +1,20 @@
+function ret = applyMask(A, mask)
+    
+	% initialize the answer to improve performance
+    ret(size(A,1),size(A,2)) = 0;
+
+	% initialize the auxA matrix with the correction of the borders
+	auxA = ones(size(A,1) + ceil(size(mask,1) / 2), size(A,2) + ceil(size(mask,2) / 2)) * 128;
+	auxA(1:size(A,1), 1:size(A,2)) = A;
+
+	for i = 1 : size(A,1)
+		for j = 1 : size(A,2)
+			
+			% get the submatrix to apply the mask to 
+			subA = auxA(i:i + size(mask,1) - 1,j:j + size(mask,2) - 1);
+			ret(i,j) = sum(sum(double(subA) .* mask));
+		end
+	end
+	
+	ret = uint8(ret);
+end
