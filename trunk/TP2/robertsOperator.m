331192610648baa7906674e9b27a3263857f359d
cliksmodernos@gmail.com <cliksmodernos@gmail.com@cba955b3-617a-336c-4ae1-095d0d3084ae>
2010-09-14 21:31:50 +0000

    Cambie unos nombres, y agregue unas implementaciones nuevas de los filtros pasa bajos y altos.

---
+++ trunk/TP2/robertsOperator.m
@@ -0,0 +1,22 @@
+function ret = robertsOperator(img)
+
+	mask1 = [1, 0; 0, -1];
+	mask2 = [0, 1; -1, 0];
+
+	% process for RGB or RAW image
+	if(length(size(img.full)) == 3)
+		ret.R = applyMask(img.R, mask1);
+		ret.R = imageSum(ret.R, applyMask(img.R, mask2));
+		ret.G = applyMask(img.G, mask1);
+		ret.G = imageSum(ret.G, applyMask(img.G, mask2));
+		ret.B = applyMask(img.B, mask1);
+		ret.B = imageSum(ret.B, applyMask(img.B, mask2));
+		ret.full = ret.R;
+		ret.full(:,:,2) = ret.G;
+		ret.full(:,:,3) = ret.B;
+	else
+		ret = img;
+		ret.full = applyMask(ret.full, mask1);
+		ret.full = imageSum(ret.full, applyMask(ret.full, mask2));
+	end
+end
