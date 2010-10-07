c8952c236744e4bdf500d72f128bc4df141cb070
cliksmodernos@gmail.com <cliksmodernos@gmail.com@cba955b3-617a-336c-4ae1-095d0d3084ae>
2010-10-07 22:09:21 +0000

    LoG hecho y haugh a medio terminar

--- trunk/TP3/LOGFilter.m
+++ trunk/TP3/LOGFilter.m
@@ -1,9 +1,17 @@
-function ret = LOGFilter(img)
+function ret = LOGFilter(img, sigma, dim)
 	
 	% the return value type is the same as the entry image type
 	ret = img;
 	
 	% FILMINA 32!!!!
+	dim = (dim - 1) / 2;
+	[x, y] = meshgrid(-dim:dim, -dim:dim);
+
+	LOG = -1 / (sqrt(2*pi) * sigma*sigma*sigma) * (2 - (x.*x + y.*y) / (sigma*sigma)) .* exp(-(x.*x + y.*y) / (2*sigma*sigma));
+	
+	% normalize it...
+	mask = LOG / sum(sum(LOG));
+	ret.LoG = mask;
 	
 	% process for RGB or RAW image
 	if(length(size(img.full)) == 3)
@@ -14,7 +22,6 @@ function ret = LOGFilter(img)
 		ret.full(:,:,2) = ret.G;
 		ret.full(:,:,3) = ret.B;
 	else
-		ret = img;
 		ret.full = applyMask(ret.full, mask);
 	end
 end
