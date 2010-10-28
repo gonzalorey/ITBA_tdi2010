636df8f21459967f5990f52e2c741b9f7f216506
cliksmodernos <cliksmodernos@gmail.com>
2010-10-28 20:19:52 +0000

    Agregadas las funciones de la fft.

---
+++ TP4/applyFFT.m
@@ -0,0 +1,10 @@
+function ret = applyFFT(img)
+	ret.orig = img.full;
+	ret.height = img.height;
+	ret.width = img.width;
+	ret.fft = fftshift(fft2(img.full));
+	ret.real = real(ret.fft);
+	ret.imag = imag(ret.fft);
+	ret.module = sqrt(ret.real .^2 + ret.imag .^2);
+	ret.phase = atan(ret.imag ./ ret.real);
+end
